31f1483fa1ed5ce3c62d09771e27739c6a314920
Arman0701 <gittadevosyan889@gmail.com>
2022-04-11 17:36:04 +0400

    fix: Fixing solution in Task 3.

--- MostRepeatedElement.js
+++ MostRepeatedElement.js
@@ -1,15 +1,19 @@
 "use strict";
 
 function repeatedElems(arr){
+    let target;
     let max = 1;
     const newArr = [];
     arr.forEach(elem => {
         if (newArr[elem] !== undefined) {
             newArr[elem] += 1;
-            if (newArr[elem] > max) max = newArr[elem];
+            if (newArr[elem] > max){
+                target = elem;
+                max = newArr[elem];
+            }
         } else newArr[elem] = 1;
     });
-    return max;
+    return target;
 }
 
 console.log(repeatedElems([1,23,8,78,78,12,12,13,13,13,15]));
